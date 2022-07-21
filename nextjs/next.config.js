9c93c34731bccd65b4d7bcd3fa29ad38379ad4be
Nadeem Ali <51571231+virtualnadeem@users.noreply.github.com>
2022-07-21 13:47:38 +0530

    Update next.config.js

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -16,4 +16,11 @@ module.exports = {
       },
     ]
   },
+  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  
 }
